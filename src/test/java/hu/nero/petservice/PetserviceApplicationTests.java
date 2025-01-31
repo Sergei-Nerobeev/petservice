c480c70ff9dd02576aa44ea552ee983fbe151a7b
coder <sergeinerobeev@gmail.com>
2025-01-31 19:35:21 +0100

    Initial commit

---
+++ src/test/java/hu/nero/petservice/PetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nero.petservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
